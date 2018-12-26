fb062044d70ed1046a01f00e104e8ea2d4b41947
git config --global user.name TNorsang <thenorsang@gmail.com>
2018-12-26 11:37:19 -0500

    add game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,25 +8,72 @@ $(document).ready(function () {
 
     // ============== Creates button from the arrays ================= \\
 
-    for (var i = 0; i < topics.length; i++) {
-        $('.dogTypes').append('<button>' + topics[i] + '</button>');
+    function displayDogInfo() {
+
+        var dogBreed = $(this).attr('data-name');
+        console.log(this);
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=Jm0Lnl7Ny3f7ljroOhENuKSTWvuYo24i&q=' + dogBreed + '&limit=25&offset=0&rating=G&lang=en';
+        var debugResposne;
+
+
+        // ============== Getting the API using AJAX ================= \\
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var imageURL = response.imageURL;
+
+            var image = $('<img>').attr('src', imgURL);
+
+            debugResposne = response;
+            console.log(response);
+
+        });
     }
 
-    // ============== Getting the API using AJAX ================= \\
-    var queryURL = 'https://api.giphy.com/v1/gifs/random?api_key=Jm0Lnl7Ny3f7ljroOhENuKSTWvuYo24i&tag=dogs';
-    var debugResposne;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
 
-    }).then(function (response) {
-        debugResposne = response;
-        console.log(response);
-    });
+    // ============== Setting the var queryURL and debugResponse ================= \\
+
+
+
+    function renderButtons() {
+
+        $('#dogTypes').empty();
+
+        for (var i = 0; i < topics.length; i++) {
+            var a = $('<button>');
+            a.addClass('topic-btn');
+            a.attr('data-name', topics[i]);
+            a.text(topics[i]);
+            $('.dogTypes').append(a);
+
+            // $('.dogTypes').append('<button>'  + topics[i] + '</button>');
+
+        }
+
+    };
+
+
+    renderButtons();
+
+
+    // ============== Getting the API using AJAX ================= \\
+
+    $('#dogTypes').on('click', function (buttons) {
+        var dogType = $('#dogTypes').val().trim();
+        topics.push(dogType);
+
+
+    })
+
+
+
 
     // ============== Displays the image on DOM ================= \\
 
-    
+
 
     // ============== waits for the DOM to load ================= \\
 
